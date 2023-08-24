0ef60baff6a6663fc277eebc4daa65453945fe3e
Rich Barnes <locklear904@gmail.com>
2023-08-24 10:59:57 -0400

    Implement function to display books
    Create function that dynamically displays information from the
    myLibrary array Book objects onto the page.

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ const bookRead = document.getElementById('bookRead');
 const bookButton = document.getElementById('bookButton');
 const bookForm = document.getElementById('bookForm');
 const myLibrary = [];
+const main = document.getElementById('main');
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -22,6 +23,51 @@ function addBookToLibrary() {
     bookForm.reset();
 }
 
+function displayBooks(book) {
+    let bookCard = document.createElement("div");
+    bookCard.classList.add('bookCard');
+    
+    let cardTitle = document.createElement("h3");
+    cardTitle.classList.add('cardTitle');
+    bookCard.appendChild(cardTitle);
+    cardTitle.textContent = `Title: ${book.title}`;
+
+    let cardAuthor = document.createElement("p");
+    cardAuthor.classList.add('cardAuthor');
+    bookCard.appendChild(cardAuthor);
+    cardAuthor.textContent = `Author: ${book.author}`;
+
+    let cardPages = document.createElement("p");
+    cardPages.classList.add('cardPages');
+    bookCard.appendChild(cardPages);
+    cardPages.textContent = `Pages: ${book.pages}`;
+
+    let cardReadDiv = document.createElement("div");
+    cardReadDiv.classList.add('cardReadDiv');
+    bookCard.appendChild(cardReadDiv);
+
+    let cardReadLabel = document.createElement("label");
+    cardReadLabel.classList.add('cardReadLabel');
+    cardReadDiv.appendChild(cardReadLabel);
+    cardReadLabel.textContent = "Check if read:    "
+
+    let cardRead = document.createElement("input");
+    cardRead.classList.add('cardRead');
+    cardRead.type = "checkbox";
+    cardRead.checked = book.read;
+    cardReadDiv.appendChild(cardRead);
+    cardRead.textContent = "Check if read: "
+
+    main.appendChild(bookCard);
+}
+
 bookButton.addEventListener('click', addBookToLibrary);
 
-const theHobbit = new Book("The Hobbit", "JRR Tolkien", "295", "not read yet");
+const theHobbit = new Book("The Hobbit", "JRR Tolkien", "295", true);
+const testBook = new Book("Test Book", "Test Author", "199", false);
+const testBook2 = new Book("Test Book 2", "Test Author 2", "2", true);
+myLibrary.push(theHobbit, testBook, testBook2);
+
+myLibrary.forEach((book) => {
+    displayBooks(book);
+});
